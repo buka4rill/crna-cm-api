eb8035254c5614e0a11ebfcd2a194453b14a4207
Ebuka Abraham <buka4rill@gmail.com>
2021-05-13 14:23:48 +0100

    more configs for prod

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -5,7 +5,9 @@ import path from "path";
 import { __prod__ } from "./constants";
 import { Course } from "./entities/Course";
 import { AppUser } from "./entities/AppUser";
-import { database as db } from './config';
+// import { database as db } from './config';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default {
   migrations: {
@@ -13,10 +15,10 @@ export default {
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
   entities: [AppUser, Course],
-  dbName: 'carnadb',
-  user: 'postgres',
-  password: 'postgres',
+  dbName: __prod__ ? process.env.DATABASE : 'carnadb',
+  user: __prod__ ? process.env.USER : 'postgres',
+  password: __prod__ ? process.env.PASSWORD : 'postgres',
   debug: !__prod__,
   type: 'postgresql',
-  clientUrl: __prod__ ? db.DATABASE_URL : undefined,
+  clientUrl: process.env.DATABASE_URL,
 } as Parameters<typeof MikroORM.init>[0];
